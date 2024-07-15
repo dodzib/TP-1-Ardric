7ae21c1f5c54ef1276f10cb349bd7e8313bb7f4f
Ardric KINDJI <ardric85@gmail.com>
2024-07-15 19:52:26 -0400

    test offline

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v3';
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
     "offline",
-    "index.html",
+   
 
 
 ];
@@ -52,4 +52,20 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
  console.log('[ServiceWorker] Fetch', evt.request.url);
  //Add fetch event handler here.
+
+ if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+        return;
+        }
+            evt.respondWith(
+             fetch(evt.request)
+                .catch(() => {
+                    return caches.open(CACHE_NAME)
+                    .then((cache) => {
+             return cache.match('/Cochenille/PointNClick/offline.html' );
+    });
+    })
+    );
+   
+
 });
